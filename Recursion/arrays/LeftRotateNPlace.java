78167541c3670ec3e00910dbf7eeb2fc3fe9fc9d
Jyoti12345 <jyotikumarichaursiya@gmail.com>
2024-08-12 12:49:22 +0530

    Arrays problem

--- Recursion/arrays/LeftRotateNPlace.java
+++ Recursion/arrays/LeftRotateNPlace.java
@@ -20,6 +20,25 @@ public class LeftRotateNPlace {
 
        }
    }
+   
+   
+   static void leftRotateOptoimalSoln(int arr,int n,int d){
+       
+       int start=0;int end=n-1;
+       
+       reverse(arr,0,);
+   }
+
+   static void reverse(int arr[],int start,int end){
+
+       while(start<=end){
+           int temp=arr[start];
+           arr[start]=arr[end];
+           arr[end]=temp;
+           start++;
+           end--;
+       }
+   }
 
     public static void main(String[] args) {
         int arr[]={1,2,3,4,5,6,7};
